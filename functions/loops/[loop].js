95f56094ef408a92426f4abd6b7177d2fe5f5163
Julian Prokay <jprokay@gmail.com>
2024-01-13 23:34:58 -0800

    feat: add basic resume

--- functions/loops/[loop].js
+++
@@ -1,6 +0,0 @@
-export async function onRequestGet(context) {
-  const loop = await context.env.LOOPS.get(context.params.loop)
-  console.log('Loop: ', context.params.loop, ' Body: ', loop)
-
-  return new Response(loop)
-}
--- public/session.js
+++
@@ -1,7 +0,0 @@
-async function loadSession() {
-  const resp = await fetch('/users/new', { credentials: 'include'})
-  console.log('cookie', resp.headers)
-  console.log('cookie', Array.from(resp.headers.entries()))
-}
-
-loadSession()
